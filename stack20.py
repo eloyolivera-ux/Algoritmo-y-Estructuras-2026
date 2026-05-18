ecf51ea2241138acd04ff8a9b0500225fa815f7d
eloyolivera-ux <eloyolivera119@gmail.com>
2026-05-18 19:12:53 -0300

    Add files via upload

---
+++ stack20.py
@@ -0,0 +1,77 @@
+from typing import Any
+
+
+# Clase Stack
+class Stack:
+
+    def __init__(self):
+        self.__elements = []
+
+    def push(self, value: Any) -> None:
+        self.__elements.append(value)
+
+    def pop(self) -> Any:
+        return self.__elements.pop()
+
+    def size(self) -> int:
+        return len(self.__elements)
+
+
+# Funcion para direccion contraria
+def contrario(direccion):
+
+    if direccion == "norte":
+        return "sur"
+
+    elif direccion == "sur":
+        return "norte"
+
+    elif direccion == "este":
+        return "oeste"
+
+    elif direccion == "oeste":
+        return "este"
+
+    elif direccion == "noreste":
+        return "suroeste"
+
+    elif direccion == "noroeste":
+        return "sureste"
+
+    elif direccion == "sureste":
+        return "noroeste"
+
+    elif direccion == "suroeste":
+        return "noreste"
+
+
+# Crear pila
+movimientos = Stack()
+
+# Cantidad de movimientos
+n = input("Cantidad de movimientos: ")
+n = int(n)
+
+print("Recorrido de ida:")
+
+# Guardar movimientos
+for i in range(n):
+
+    pasos = input("Pasos: ")
+    pasos = int(pasos)
+
+    direccion = input("Direccion: ")
+
+    movimientos.push((pasos, direccion))
+
+    print("Caminar", pasos, "pasos hacia", direccion)
+
+
+# Mostrar regreso
+print("Regreso del robot:")
+
+while movimientos.size() > 0:
+
+    pasos, direccion = movimientos.pop()
+
+    print("Caminar", pasos, "pasos hacia", contrario(direccion))
